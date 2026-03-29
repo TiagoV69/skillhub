acb685b562a457b37d68e417a7d2bf498dfbf559
Alexis <oschan2004@gmail.com>
2026-03-29 00:40:21 -0500

    Commit

--- usuarios/views.py
+++ usuarios/views.py
@@ -30,7 +30,7 @@ def login(request):
 def callback(request):
     token = oauth.auth0.authorize_access_token(request)
     request.session["user"] = token
-    return redirect(request.build_absolute_uri(reverse("usuario:index")))
+    return redirect(request.build_absolute_uri(reverse("usuarios:index")))
 
 
 def logout(request):
